792c07f0d2e1ef0d15ccd466d9866d47b32300bb
Oscar Gelvez <oscargelvez23@gmail.com>
2018-10-06 16:57:19 -0500

    primer commit

---
+++ database/migrations/2018_09_22_234234_create_theories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTheoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('theories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('description');
+            $table->string('author')->nullable();
+            $table->date('publication_date')->nullable();
+            $table->string('img_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('theories');
+    }
+}
